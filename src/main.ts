24ec4d48ad51309a1881da707e9e75b49c1dd06b
izhichao <43055550@qq.com>
2022-06-10 17:48:32 +0800

    chore: 引入postcss适配移动端

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import 'normalize.css';
 import Varlet from '@varlet/ui';
 import router from './router';
+import '@varlet/ui/es/style.js'
 const app = createApp(App);
 
 app.use(router);
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [{ path: '', name: 'Home', component: () => import(/* webpackChunkName: "home" */ './views/HomeView.vue') }];
+const routes: RouteRecordRaw[] = [
+  { path: '', name: 'Home', component: () => import(/* webpackChunkName: "home" */ './views/Home/HomeView.vue') },
+  { path: '', name: 'Show', component: () => import(/* webpackChunkName: "show" */ './views/Show/ShowView.vue') },
+  { path: '', name: 'My', component: () => import(/* webpackChunkName: "my" */ './views/My/MyView.vue') }
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
