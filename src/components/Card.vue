37b0e7e5a0625996a9decde32edba2d15b2d5641
izhichao <43055550@qq.com>
2022-07-21 21:30:48 +0800

    feat: 封装Card组件；完成图表卡片与最近交易卡片布局

---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <div class="card__header">
+      <div class="card__header__title">{{ title }}</div>
+      <div class="card__header__more">></div>
+    </div>
+    <div class="card__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.card {
+  padding: 15px;
+  border-radius: 20px;
+  background: #fff;
+
+  &:not(:nth-of-type(1)) {
+    margin-top: 10px;
+  }
+
+  &__header {
+    display: flex;
+    height: 24px;
+    margin-bottom: 10px;
+    justify-content: space-between;
+    align-items: center;
+
+    &__title {
+      font-size: 14px;
+      font-weight: 700;
+    }
+  }
+}
+</style>
--- src/components/Circle.vue
+++ src/components/Circle.vue
@@ -12,6 +12,6 @@
   height: 500px;
   border-radius: 50%;
   background-image: linear-gradient(239deg, #3a6ff3 0%, #50c7fb 100%);
-  z-index: -999;
+  z-index: -1;
 }
 </style>
