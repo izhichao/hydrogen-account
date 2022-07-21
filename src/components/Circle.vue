14ab81fa8d4bf4c7919d44a03e8a413cb5505081
izhichao <43055550@qq.com>
2022-07-21 19:10:33 +0800

    feat: 新增Circle组件

---
+++ src/components/Circle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg"></div>
+</template>
+
+<style lang="less" scoped>
+.bg {
+  position: absolute;
+  left: 50%;
+  top: -350px;
+  transform: translate(-50%);
+  width: 800px;
+  height: 500px;
+  border-radius: 50%;
+  background-image: linear-gradient(239deg, #3a6ff3 0%, #50c7fb 100%);
+  z-index: -999;
+}
+</style>
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
-  首页
+  <Circle></Circle>
+
+  <div class="main-content">
+    <div class="month">1</div>
+
+    <div class="recent">2</div>
+  </div>
   <Docker :currentIndex="0"></Docker>
 </template>
 
 <script lang="ts" setup>
+import Circle from '../../components/Circle.vue';
 import Docker from '../../components/Docker.vue';
 </script>
 
