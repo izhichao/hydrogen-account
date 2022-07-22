09aa6308293e8777260c7920d505e273e7578ebf
izhichao <43055550@qq.com>
2022-07-22 11:10:14 +0800

    feat: 封装ChartCard组件

---
+++ src/components/ChartCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <Card :title="title">
+    <div class="price">-123.01</div>
+    <div class="chart"></div>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+import Card from './Card.vue';
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.price {
+  font-size: 20px;
+  font-weight: 700;
+  color: #ff6c7a;
+  margin-bottom: 10px;
+}
+
+.chart {
+  height: 130px;
+  background-color: #ccc;
+}
+</style>
