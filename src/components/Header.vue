a3d598d261991a72e543dfd5ee010055a8ddec5d
izhichao <43055550@qq.com>
2022-07-22 12:08:44 +0800

    feat: 封装Header组件

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div class="header__title">氢记账</div>
+    <div class="header__search">搜索</div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped>
+.header {
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  padding: 0 10px;
+
+  &__title {
+    font-size: 18px;
+  }
+}
+</style>
