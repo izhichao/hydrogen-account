627320d93e693f5c574eaf1ddaeb0bf9bf9ac1f2
izhichao <60996631+izhichao@users.noreply.github.com>
2022-07-23 15:38:58 +0800

    feat: 首页弹窗按钮

---
+++ src/components/Calculator.vue
@@ -0,0 +1,32 @@
+<template>
+  <var-popup position="bottom" v-model:show="show">
+    <div class="popup-example-block"></div>
+  </var-popup>
+</template>
+
+<script lang="ts" setup>
+import { watch } from 'vue';
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false
+  }
+});
+
+// 双向绑定状态
+const emits = defineEmits(['update:show']);
+watch(
+  () => props.show,
+  (newVal) => {
+    emits('update:show', newVal);
+  }
+);
+</script>
+
+<style lang="less" scoped>
+.popup-example-block {
+  padding: 20px 24px;
+  width: 250px;
+}
+</style>
