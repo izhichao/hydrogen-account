23316d4ebd46d0459ff23e0a2a72cb01a1fff957
izhichao <43055550@qq.com>
2022-07-23 15:38:58 +0800

    feat: 首页弹窗按钮

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -10,6 +10,10 @@
       <ListCard title="所有账户" :list="accountList"></ListCard>
     </div>
   </div>
+  <var-button class="btn" type="primary" size="large" round @click="handlePop">
+    <var-icon name="plus" :size="26" />
+  </var-button>
+  <Calculator v-model:show="calcStatus"></Calculator>
   <Docker :currentIndex="0"></Docker>
 </template>
 
@@ -19,6 +23,8 @@ import ChartCard from '../../components/ChartCard.vue';
 import ListCard, { Deal } from '../../components/ListCard.vue';
 import Circle from '../../components/Circle.vue';
 import Docker from '../../components/Docker.vue';
+import Calculator from '../../components/Calculator.vue';
+import { ref } from 'vue';
 
 const recentDealList: Deal[] = [
   { id: 0, title: '购物', desc: '手机壳', amount: -4.58 },
@@ -30,12 +36,28 @@ const accountList: Deal[] = [
   { id: 0, title: '支付宝', amount: 5000 },
   { id: 1, title: '微信', amount: 5000 }
 ];
+
+const calcStatus = ref(false)
+const handlePop = () => {
+  calcStatus.value = true;
+}
 </script>
 
 <style lang="less" scoped>
+@import '../../style/variables.less';
+
 .header {
   &__search {
     font-size: 20px;
   }
+}
+
+.btn {
+  position: absolute;
+  background-color: @themeColor;
+  height: 40px;
+  width: 40px;
+  right: 25px;
+  bottom: 75px;
 }
 </style>
