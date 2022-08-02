05b6c47506b7f8fd63ee7ed83193187a4137faa9
izhichao <43055550@qq.com>
2022-08-02 16:14:01 +0800

    refactor: 抽离账户相关逻辑代码

--- src/composables/useAccount.ts
+++ src/composables/useAccount.ts
@@ -1,2 +1,74 @@
+import { Dialog, Snackbar } from '@varlet/ui';
+import { storeToRefs } from 'pinia';
+import { reactive } from 'vue';
+import { useAccountStore } from '../store/useAccountStore';
 
-export const useAccount = () => {};
+export const useAccount = () => {
+  const accountStore = useAccountStore();
+  const { accountList, accountAmount, totalAsset } = storeToRefs(accountStore);
+  const { addAccount, findAccount, editAccount, deleteAccount } = accountStore;
+
+  // 新增账户
+  const addAccountModel = reactive({
+    name: '',
+    amount: '',
+    status: false
+  });
+
+  const handleShowAdd = () => {
+    addAccountModel.status = true;
+  };
+
+  const handleAdd = () => {
+    if (!addAccountModel.name || !addAccountModel.amount) {
+      Snackbar('请输入账户名与余额');
+      return;
+    }
+    addAccount(addAccountModel.name, +addAccountModel.amount);
+    addAccountModel.name = '';
+    addAccountModel.amount = '';
+  };
+
+  // 编辑账户
+  const editAccountModel = reactive({
+    id: 0,
+    name: '',
+    amount: '',
+    status: false
+  });
+
+  const handleShowEdit = (id: number) => {
+    const account = findAccount(id);
+    editAccountModel.status = true;
+    editAccountModel.id = id;
+    editAccountModel.name = account?.name as string;
+    editAccountModel.amount = account?.amount?.toString() as string;
+  };
+
+  const handleEdit = () => {
+    editAccount(editAccountModel.id, editAccountModel.name, +editAccountModel.amount);
+  };
+
+  // 删除账户
+  const handleDelete = () => {
+    Dialog('确认删除').then((res) => {
+      if (res === 'confirm') {
+        deleteAccount(editAccountModel.id);
+        editAccountModel.status = false;
+      }
+    });
+  };
+
+  return {
+    addAccountModel,
+    editAccountModel,
+    accountList,
+    accountAmount,
+    totalAsset,
+    handleShowAdd,
+    handleAdd,
+    handleShowEdit,
+    handleEdit,
+    handleDelete
+  };
+};
