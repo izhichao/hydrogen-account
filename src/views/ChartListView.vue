655efba0d70f3a89ddbe86130a98a3f83be321a7
izhichao <60996631+izhichao@users.noreply.github.com>
2022-08-05 20:56:46 +0800

    feat: 图表卡片跳转添加query参数

--- src/views/ChartListView.vue
+++ src/views/ChartListView.vue
@@ -1,8 +1,8 @@
 <template>
-  <Header title="分类统计" :back="true"></Header>
+  <Header :title="title" :back="true"></Header>
   <div class="main-content">
     <div class="card-list">
-      <ChartCard title="分类"></ChartCard>
+      <ChartCard :title="title" :more="false"></ChartCard>
       <Card :header="false" class="padding-small">
         <ListItem :item="{ id: 1, name: '购物', desc: '10笔', amount: -100 }"></ListItem>
       </Card>
@@ -15,6 +15,17 @@ import Header from '../components/Header.vue';
 import ChartCard from '../components/ChartCard.vue';
 import Card from '../components/Card.vue';
 import ListItem from '../components/ListItem.vue';
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+const route = useRoute();
+const { type, month, year } = route.query;
+
+const title = ref('分类统计');
+if (type === 'month') {
+  title.value = `${month}月支出`;
+} else if (type === 'year') {
+  title.value = `${year}年支出`;
+}
 </script>
 
 <style lang="less" scoped>
