6e2c6a90196f445fdbf0576d8020de4ce8322660
izhichao <43055550@qq.com>
2022-08-06 10:23:08 +0800

    feat: 交易数据按分类、按月分组

---
+++ src/composables/useDeal.ts
@@ -0,0 +1,59 @@
+export interface Deal {
+  id: number;
+  name: string;
+  categoryId: number;
+  desc: string;
+  amount: number;
+  date: string;
+  time: string;
+}
+
+interface DealGroup {
+  name: string;
+  total: number;
+  value: Deal[];
+}
+
+export const useDeal = () => {
+  // 将数据进行分组(按日，按月，按分类)
+  const converListToGroup = (list: Deal[], type: string) => {
+    const dealObj: { [key: string]: Deal[] } = {};
+    list.forEach((deal) => {
+      switch (type) {
+        case 'day':
+          if (dealObj[deal.date]) {
+            dealObj[deal.date].push(deal);
+          } else {
+            dealObj[deal.date] = [deal];
+          }
+          break;
+        case 'month':
+          const month = deal.date.slice(0, 7);
+          if (dealObj[month]) {
+            dealObj[month].push(deal);
+          } else {
+            dealObj[month] = [deal];
+          }
+          break;
+        case 'category':
+          if (dealObj[deal.name]) {
+            dealObj[deal.name].push(deal);
+          } else {
+            dealObj[deal.name] = [deal];
+          }
+          break;
+      }
+    });
+  };
+  // 将分组的数据转换为数组形式，并添加统计
+  const convertObjToArray = (obj: { [key: string]: Deal[] }) => {
+    const dealList: DealGroup[] = [];
+    for (const key in obj) {
+      const total = obj[key].reduce((total, currentValue) => {
+        return total + currentValue.amount;
+      }, 0);
+      dealList.push({ name: key, total, value: obj[key] });
+    }
+    return dealList;
+  };
+};
--- src/store/useCategoryStore.ts
+++ src/store/useCategoryStore.ts
@@ -4,39 +4,40 @@ import { useDealStore } from './useDealStore';
 export interface Category {
   id: number;
   name: string;
-  desc?: string;
+  desc: string;
 }
 
 export const useCategoryStore = defineStore('category', {
   state: () => {
     return {
       categoryList: [
-        { id: 0, name: '购物' },
-        { id: 1, name: '水电费' },
-        { id: 2, name: '交通' }
+        { id: 0, name: '购物', desc: '共0笔交易' },
+        { id: 1, name: '水电费', desc: '共0笔交易' },
+        { id: 2, name: '交通', desc: '共0笔交易' }
       ] as Category[]
     };
   },
   getters: {
-    categoryListWithDesc: (state) =>  {
+    // 添加交易笔数统计（更改原数组）
+    categoryListWithDesc: (state) => {
       const dealList = useDealStore().dealList;
-      const newCategoryList: Category[] = JSON.parse(JSON.stringify(state.categoryList));
-      newCategoryList.forEach((category) => {
+      // const newCategoryList: Category[] = JSON.parse(JSON.stringify(state.categoryList));
+      state.categoryList.forEach((category) => {
         let sum = 0;
         dealList.forEach((deal) => {
           if (deal.categoryId === category.id) {
             sum++;
           }
-          category.desc = `共${sum}笔交易`;
         });
+        category.desc = `共${sum}笔交易`;
       });
-      return newCategoryList;
+      return state.categoryList;
     }
   },
   actions: {
     addCategory(name: string) {
       const newId = this.categoryList[this.categoryList.length - 1].id + 1;
-      this.categoryList.push({ id: newId, name });
+      this.categoryList.push({ id: newId, name, desc: '共0笔交易' });
     },
     deleteCategory(id: number) {
       let newCategoryList = this.categoryList.filter((category) => {
@@ -54,6 +55,6 @@ export const useCategoryStore = defineStore('category', {
         return category;
       });
     }
-  }
-  // persist: true
+  },
+  persist: true
 });
