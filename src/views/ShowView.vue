286335307d55876bb0e04078aefbd7c5ad6c66a0
izhichao <60996631+izhichao@users.noreply.github.com>
2022-08-06 19:33:22 +0800

    feat: 统计页面标题支出统计

--- src/views/ShowView.vue
+++ src/views/ShowView.vue
@@ -4,9 +4,21 @@
     <Header title="统计"></Header>
 
     <div class="card-list">
-      <ChartCard title="分类统计" @more="router.push({ name: 'Chart', query: { type: 'category', month } })"></ChartCard>
-      <ChartCard :title="`${month}月支出`" @more="router.push({ name: 'Chart', query: { type: 'month', month } })"></ChartCard>
-      <ChartCard :title="`${year}年支出`" @more="router.push({ name: 'Chart', query: { type: 'year', year } })"></ChartCard>
+      <ChartCard
+        title="分类统计"
+        :amount="monthExpend"
+        @more="router.push({ name: 'Chart', query: { type: 'category', time: `${year}-${monthPad}` } })"
+      ></ChartCard>
+      <ChartCard
+        :title="`${month}月支出`"
+        :amount="monthExpend"
+        @more="router.push({ name: 'Chart', query: { type: 'month', time: `${year}-${monthPad}` } })"
+      ></ChartCard>
+      <ChartCard
+        :title="`${year}年支出`"
+        :amount="yearExpend"
+        @more="router.push({ name: 'Chart', query: { type: 'year', time: `${year}` } })"
+      ></ChartCard>
     </div>
   </div>
   <Docker :currentIndex="1"></Docker>
@@ -18,10 +30,17 @@ import Header from '../components/Header.vue';
 import ChartCard from '../components/ChartCard.vue';
 import Docker from '../components/Docker.vue';
 import { useRouter } from 'vue-router';
+import { useDealStore } from '../store/useDealStore';
+
+const { dealListGroup } = useDealStore();
+
 const router = useRouter();
 const date = new Date();
-const month = date.getMonth() + 1;
 const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const monthPad = (date.getMonth() + 1).toString().padStart(2, '0');
+const monthExpend = dealListGroup('month', `${year}-${monthPad}`)[0].total;
+const yearExpend = dealListGroup('year', `${year}`)[0].total;
 </script>
 
 <style lang="less" scoped>
