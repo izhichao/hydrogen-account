34e2ddcc4749584328fc1c9b15185d52b241698e
izhichao <43055550@qq.com>
2022-08-13 16:59:28 +0800

    feat: 新增帮助页面

---
+++ src/views/HelpView.vue
@@ -0,0 +1,12 @@
+<template>
+  <Header title="帮助" :back="true"></Header>
+</template>
+
+<script lang="ts" setup>
+import Header from '../components/Header.vue';
+</script>
+
+<style lang="less" scoped>
+@import '../style/variables.less';
+@import '../style/inner.less';
+</style>
