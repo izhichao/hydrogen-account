f6b77d3414ead0997af21bfbcc1a3b5eb0e1a489
izhichao <60996631+izhichao@users.noreply.github.com>
2022-09-06 13:22:43 +0800

    chore: vite配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0'
   },
-  base: './'
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '*': path.resolve('')
+    }
+  }
 });
