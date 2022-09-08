8c3b06f21095c2e091490274c29c9b834b34f0f9
izhichao <43055550@qq.com>
2022-09-08 17:28:45 +0800

    chore: 按需引入varlet组件库

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import Varlet from '@varlet/ui';
 import router from './router';
 import { createPinia } from 'pinia';
 import 'normalize.css';
-import '@varlet/ui/es/style.js';
 import './style/index.less';
+import '@varlet/ui/es/snackbar/style/index.js';
+import '@varlet/ui/es/dialog/style/index.js';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 app.use(router);
-app.use(Varlet);
 app.use(pinia);
 
 app.mount('#app');
