75f3a849050ff3ff810b338f4f0a3cb8e7e18cc8
izhichao <43055550@qq.com>
2022-09-17 12:20:44 +0800

    feat: 封装左右过渡动画组件

---
+++ src/components/LeftTransition.vue
@@ -0,0 +1,29 @@
+<template>
+  <Transition name="slide-right">
+    <slot></slot>
+  </Transition>
+</template>
+
+<style lang="less">
+.slide-left-leave-active,
+.slide-left-enter-active {
+  transition: all 0.5s;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.slide-left-leave-from,
+.slide-left-enter-to {
+  transform: translateX(0);
+}
+
+.slide-left-leave-to {
+  transform: translateX(-100%);
+}
+
+.slide-left-enter-from {
+  transform: translateX(100%);
+}
+</style>
