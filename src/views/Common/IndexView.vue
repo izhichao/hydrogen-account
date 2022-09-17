7a8fee6db235d1d16a7c8d9f352f6771be5117e6
izhichao <43055550@qq.com>
2022-09-17 15:16:26 +0800

    fix: 修复Docker栏高亮不随页面变化的bug

--- src/views/Common/IndexView.vue
+++ src/views/Common/IndexView.vue
@@ -4,27 +4,24 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import Docker from '../../components/Docker.vue';
 const route = useRoute();
 const index = ref(0);
-watch(
-  () => route.name,
-  (val) => {
-    switch (val) {
-      case 'Home':
-        index.value = 0;
-        break;
-      case 'Show':
-        index.value = 1;
-        break;
-      case 'My':
-        index.value = 2;
-        break;
-    }
-  }
-);
+
+switch (route.name) {
+  case 'Home':
+    index.value = 0;
+    break;
+  case 'Show':
+    index.value = 1;
+    break;
+  case 'My':
+    index.value = 2;
+    break;
+}
+
 </script>
 
 <style lang="less" scoped></style>
