bb9af51fee2387e0f7b2bfe444ee6cb0fd909e55
izhichao <60996631+izhichao@users.noreply.github.com>
2022-09-17 15:15:58 +0800

    feat: 子页面添加过渡动画；移除过渡组件

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,80 @@
 <template>
-  <router-view></router-view>
+  <router-view v-slot="{ Component, route }">
+    <Transition :name="transition">
+      <div :key="route.name">
+        <component :is="Component"></component>
+      </div>
+    </Transition>
+  </router-view>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const transition = ref('slide-left');
+const router = useRouter();
+
+router.afterEach((to, from) => {
+  if (!from.name) {
+    transition.value = '';
+    return;
+  }
+  if (['Home', 'Show', 'My'].includes(from.name as string) && ['Home', 'Show', 'My'].includes(to.name as string)) {
+    transition.value = '';
+    return;
+  }
+
+  if ((to.meta.index as number) > (from.meta.index as number)) {
+    transition.value = 'slide-right';
+  } else {
+    transition.value = 'slide-left';
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.slide-left-leave-active,
+.slide-left-enter-active {
+  transition: all 0.3s;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.slide-left-leave-from,
+.slide-left-enter-to {
+  transform: translateX(0);
+}
+
+.slide-left-leave-to {
+  transform: translateX(-100%);
+}
+
+.slide-left-enter-from {
+  transform: translateX(100%);
+}
+
+.slide-right-leave-active,
+.slide-right-enter-active {
+  transition: all 0.3s;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.slide-right-leave-from,
+.slide-right-enter-to {
+  transform: translateX(0);
+}
+
+.slide-right-leave-to {
+  transform: translateX(100%);
+}
+
+.slide-right-enter-from {
+  transform: translateX(-100%);
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -9,37 +9,72 @@ const routes: RouteRecordRaw[] = [
       {
         path: '',
         name: 'Home',
-        component: () => import(/* webpackChunkName: "home" */ './views/Home/HomeView.vue')
+        component: () => import(/* webpackChunkName: "home" */ './views/Home/HomeView.vue'),
+        meta: {
+          index: 0
+        }
       },
       {
         path: 'show',
         name: 'Show',
-        component: () => import(/* webpackChunkName: "show" */ './views/Show/ShowView.vue')
+        component: () => import(/* webpackChunkName: "show" */ './views/Show/ShowView.vue'),
+        meta: {
+          index: 1
+        }
       },
-      { path: 'my', name: 'My', component: () => import(/* webpackChunkName: "my" */ './views/My/MyView.vue') }
-    ]
-  },
-  {
-    path: '/list',
-    name: 'List',
-    component: () => import(/* webpackChunkName: "list" */ './views/Common/ListView.vue')
+      {
+        path: 'my',
+        name: 'My',
+        component: () => import(/* webpackChunkName: "my" */ './views/My/MyView.vue'),
+        meta: {
+          index: 2
+        }
+      }
+    ],
+    meta: {
+      index: 999
+    }
   },
   {
     path: '/chart',
     name: 'Chart',
-    component: () => import(/* webpackChunkName: "chart" */ './views/Common/ChartListView.vue')
+    component: () => import(/* webpackChunkName: "chart" */ './views/Common/ChartListView.vue'),
+    meta: {
+      index: -1
+    }
+  },
+  {
+    path: '/list',
+    name: 'List',
+    component: () => import(/* webpackChunkName: "list" */ './views/Common/ListView.vue'),
+    meta: {
+      index: -2
+    }
   },
   {
     path: '/detail',
     name: 'Detail',
-    component: () => import(/* webpackChunkName: "detail" */ './views/Common/DetailView.vue')
+    component: () => import(/* webpackChunkName: "detail" */ './views/Common/DetailView.vue'),
+    meta: {
+      index: -3
+    }
   },
   {
     path: '/category',
     name: 'Category',
-    component: () => import(/* webpackChunkName: "category" */ './views/My/CategoryView.vue')
+    component: () => import(/* webpackChunkName: "category" */ './views/My/CategoryView.vue'),
+    meta: {
+      index: -1
+    }
   },
-  { path: '/help', name: 'Help', component: () => import(/* webpackChunkName: "help" */ './views/My/HelpView.vue') }
+  {
+    path: '/help',
+    name: 'Help',
+    component: () => import(/* webpackChunkName: "help" */ './views/My/HelpView.vue'),
+    meta: {
+      index: -1
+    }
+  }
 ];
 
 const router = createRouter({
