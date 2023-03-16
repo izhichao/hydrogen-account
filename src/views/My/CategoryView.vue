68c1472d7b00a18fa43169e5d70f6dd297ec205c
izhichao <60996631+izhichao@users.noreply.github.com>
2023-03-16 22:32:23 +0800

    refactor: 重构分类页面功能

--- src/views/My/CategoryView.vue
+++ src/views/My/CategoryView.vue
@@ -27,12 +27,66 @@
 </template>
 
 <script lang="ts" setup>
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { Dialog, Snackbar } from '@varlet/ui';
 import Header from '../../components/Header.vue';
 import Card from '../../components/Card.vue';
 import ListItem from '../../components/ListItem.vue';
-import { useCategory } from '../../composables/useCategory';
-const { categoryListWithDesc, name, editModel, handleAdd, handleDelete, handleEdit, handleShow, handlePush } =
-  useCategory();
+import { useCategoryStore } from '../../store/useCategoryStore';
+const categoryStore = useCategoryStore();
+// 获取store中的数据及函数
+const { categoryListWithDesc } = storeToRefs(categoryStore);
+const { addCategory, deleteCategory, editCategory } = categoryStore;
+
+// 新增分类
+const name = ref('');
+const handleAdd = () => {
+  if (!name.value) {
+    Snackbar('请输入分类名称');
+    return;
+  }
+  addCategory(name.value);
+  name.value = '';
+};
+
+// 编辑分类名称
+const editModel = reactive({
+  id: 0,
+  name: '',
+  status: false
+});
+
+const handleShow = (id: number) => {
+  editModel.status = true;
+  editModel.id = id;
+};
+
+const handleEdit = () => {
+  if (!editModel.name) {
+    Snackbar('请输入新的分类名称');
+    return;
+  }
+  editCategory(editModel.id, editModel.name);
+  editModel.name = '';
+  editModel.status = false;
+};
+
+// 删除分类
+const handleDelete = (id: number) => {
+  Dialog('确认删除').then((res) => {
+    if (res === 'confirm') {
+      deleteCategory(id);
+    }
+  });
+};
+
+// 展示该分类下的所有交易
+const router = useRouter();
+const handlePush = (name: string) => {
+  router.push({ name: 'List', query: { list: 'deal', keyword: name } });
+};
 </script>
 
 <style lang="less" scoped>
