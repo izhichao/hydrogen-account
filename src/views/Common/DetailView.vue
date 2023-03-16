5fed5779318713258aa96bde62c614e9a04894f4
izhichao <60996631+izhichao@users.noreply.github.com>
2023-03-16 22:39:13 +0800

    refactor: 重构交易页面功能

--- src/views/Common/DetailView.vue
+++ src/views/Common/DetailView.vue
@@ -42,7 +42,14 @@
             </div>
             <div>
               <div class="form__title">备注</div>
-              <var-input class="form__content__input" :hint="false" :line="false" placeholder="轻敲添加备注" v-model="dealModel.desc" clearable />
+              <var-input
+                class="form__content__input"
+                :hint="false"
+                :line="false"
+                placeholder="轻敲添加备注"
+                v-model="dealModel.desc"
+                clearable
+              />
             </div>
             <div>
               <var-button type="primary" block class="form__btn" @click="handleEdit" v-if="type === 'edit'">
@@ -80,21 +87,92 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { Snackbar, Dialog } from '@varlet/ui';
 import Header from '../../components/Header.vue';
 import Card from '../../components/Card.vue';
 import Calculator from '../../components/Calculator.vue';
-import { useDeal } from '../../composables/useDeal';
-const { dealModel, categoryListWithDesc, type, handleUpdateAmount, handleAdd, handleEdit, handleDelete } = useDeal();
+import { useCategoryStore } from '../../store/useCategoryStore';
+import { useDealStore } from '../../store/useDealStore';
+import getNow from '../../utils/getNow';
 
+// 时间与日期选择器
 const showDate = ref(false);
 const showTime = ref(false);
-const calculatorRef = ref();
 
 const handleDate = () => {
   setTimeout(() => {
     showDate.value = false;
   }, 50);
+};
+
+const route = useRoute();
+const router = useRouter();
+
+const categoryStore = useCategoryStore();
+const { categoryListWithDesc } = storeToRefs(categoryStore);
+const dealStore = useDealStore();
+const { findDeal, addDeal, editDeal, deleteDeal } = dealStore;
+
+const { type, id, result } = route.query;
+
+// 获取当前时间
+const { yearStr, monthStr, dayStr, hourStr, minuteStr } = getNow();
+
+const dealModel = reactive({
+  amount: (result as string) || '0',
+  categoryId: 0,
+  date: `${yearStr}-${monthStr}-${dayStr}`,
+  time: `${hourStr}:${minuteStr}`,
+  desc: ''
+});
+
+// 获取当前交易信息
+if (type === 'edit') {
+  const deal = findDeal(+(id as string));
+  dealModel.amount = (-(deal?.amount as number)).toString();
+  dealModel.categoryId = deal?.categoryId as number;
+  dealModel.date = deal?.date as string;
+  dealModel.time = deal?.time as string;
+  dealModel.desc = deal?.desc as string;
+}
+
+// 计算器组件 点击确认后更新金额
+const calculatorRef = ref();
+const handleUpdateAmount = (result: string) => {
+  dealModel.amount = result;
+};
+
+// 添加交易
+const handleAdd = () => {
+  if (+dealModel.amount <= 0) {
+    Snackbar('请输入正确的金额');
+    return;
+  }
+  addDeal(dealModel.categoryId, dealModel.desc, +dealModel.amount, dealModel.date, dealModel.time);
+  router.push({ name: 'Home' });
+};
+
+// 编辑交易
+const handleEdit = () => {
+  if (+dealModel.amount <= 0) {
+    Snackbar('请输入正确的金额');
+    return;
+  }
+  editDeal(+(id as string), dealModel.categoryId, dealModel.desc, +dealModel.amount, dealModel.date, dealModel.time);
+  router.go(-1);
+};
+
+// 删除交易
+const handleDelete = () => {
+  Dialog('确认删除').then((res) => {
+    if (res === 'confirm') {
+      deleteDeal(+(id as string));
+      router.push({ name: 'Home' });
+    }
+  });
 };
 </script>
 
