9900924dce3dc20199eb6219e3591709a887a74e
izhichao <60996631+izhichao@users.noreply.github.com>
2023-09-21 19:45:14 +0800

    feat: 分类中添加收入

--- src/store/useCategoryStore.ts
+++ src/store/useCategoryStore.ts
@@ -5,27 +5,43 @@ export const useCategoryStore = defineStore('category', {
   state: () => {
     return {
       categoryList: [
-        { id: 0, name: '餐饮' },
-        { id: 1, name: '购物' },
-        { id: 2, name: '水电费' },
-        { id: 3, name: '交通' }
+        { id: 0, name: '餐饮', type: 'out' },
+        { id: 1, name: '购物', type: 'out' },
+        { id: 2, name: '水电费', type: 'out' },
+        { id: 3, name: '交通', type: 'out' },
+        { id: 4, name: '工资', type: 'in' },
+        { id: 5, name: '股票分红', type: 'in' }
       ] as BaseCategory[]
     };
   },
   getters: {
     // 添加交易笔数统计
-    categoryListWithDesc: (state) => {
+    categoryLists: (state) => {
       const dealList = useDealStore().dealList;
-      return state.categoryList.map((category) => {
+      const list = state.categoryList.map((category) => {
         const sum = dealList.filter((deal) => deal.categoryId === category.id).length;
         return { ...category, desc: `共${sum}笔交易` };
-      }) as Category[];
+      });
+      // 按收入和支出分类
+      const categoryLists = list.reduce(
+        (result: { in: Category[]; out: Category[] }, category: Category) => {
+          if (category.type === 'in') {
+            result.in.push(category);
+          } else if (category.type === 'out') {
+            result.out.push(category);
+          }
+          return result;
+        },
+        { out: [], in: [] }
+      );
+
+      return categoryLists;
     }
   },
   actions: {
-    addCategory(name: string) {
+    addCategory(name: string, type: 'in' | 'out') {
       const newId = this.categoryList[this.categoryList.length - 1].id + 1;
-      this.categoryList.push({ id: newId, name });
+      this.categoryList.push({ id: newId, name, type });
     },
     deleteCategory(id: number) {
       this.categoryList.filter((category) => category.id !== id);
